2814385fa564fe473cd98f583703cee2a60b3070
kimbumsu0312 <91525897+kimbumsu0312@users.noreply.github.com>
2025-06-30 15:47:44 +0900

    게임 인스턴스 및 관련 내용 추가

---
+++ Framework/Engine/Public/Object_Manager.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "Base.h"
+
+NS_BEGIN(Engine)
+class CObject_Manager final : public CBase
+{
+private:
+	CObject_Manager();
+	virtual ~CObject_Manager() = default;
+
+public:
+	class CComponent*			Get_Component(_uint iLayerLevelIndex, const _wstring& strLayerTag, const _wstring& strComponentTag, _uint iIndex = 0);
+
+public:
+	HRESULT						Initialize(_uint iNumLevels);
+	HRESULT						Add_GameObject_ToLayer(_uint iLayerLevelIndex, const _wstring& strLayerTag, _uint iPrototypeLevelIndex, const _wstring& strPrototypeTag, void* pArg);
+	void						Priority_Update(_float fTimeDelta);
+	void						Update(_float fTimeDelta);
+	void						Late_Update(_float fTimeDelta);
+	void						Clear(_uint iLevelIndex);
+
+private:
+	class CGameInstance*		m_pGameInstance = { nullptr };
+	
+	_uint						m_iNumLevels = {};
+
+	map<const _wstring,
+		class CLayer*>*			m_pLayers = {};
+
+private:
+	class CLayer*				Find_Layer(_uint iLayerLevelIndex, const _wstring& strLayerTag);
+
+public:
+	static CObject_Manager*		Create(_uint iNumLevels);
+	virtual void				Free() override;
+};
+NS_END
--- Framework/Engine/Public/stdafx.h
+++ Framework/Engine/Public/stdafx.h
@@ -2,5 +2,5 @@
 #include "Timer.h"
 #include "Timer_Manager.h"
 #include "Graphic_Device.h"
-
+#include "Engine_Defines.h"
 
