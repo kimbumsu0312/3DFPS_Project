02a3bf7a2724cd4d74355df49721b164cad3dba7
kimbumsu0312 <91525897+kimbumsu0312@users.noreply.github.com>
2025-06-30 15:47:44 +0900

    게임 인스턴스 및 관련 내용 추가

--- Framework/Engine/Private/GameInstance.cpp
+++ Framework/Engine/Private/GameInstance.cpp
@@ -3,6 +3,10 @@
 
 #include "Graphic_Device.h"
 #include "Timer_Manager.h"
+#include "Level_Manager.h"
+#include "Object_Manager.h"
+#include "Prototype_Manager.h"
+#include "Renderer.h"
 
 IMPLEMENT_SINGLETON(CGameInstance)
 
@@ -21,11 +25,16 @@ HRESULT CGameInstance::Initialize_Engine(const ENGINE_DESC& EngineDesc, ID3D11De
 	if (nullptr == m_pTimer_Manager)
 		return E_FAIL;
 
+	m_pLevel_Manager = CLevel_Manager::Create();
+	if (nullptr == m_pLevel_Manager)
+		return E_FAIL;
+
 	return S_OK;
 }
 
 void CGameInstance::Update_Engine(_float fTimeDelta)
 {
+	m_pLevel_Manager->Update(fTimeDelta);
 }
 
 HRESULT CGameInstance::Clear_Resources(_uint iClearLevelID)
@@ -45,6 +54,12 @@ void CGameInstance::Render_Begin(const _float4* pClearColor)
 
 HRESULT CGameInstance::Draw()
 {
+	if (nullptr == m_pLevel_Manager)
+		return E_FAIL;
+
+	if (FAILED(m_pLevel_Manager->Render()))
+		return E_FAIL;
+
 	return S_OK;
 }
 
@@ -82,9 +97,54 @@ void CGameInstance::Compute_TimeDelta(const _wstring& strTimerTag)
 	m_pTimer_Manager->Compute_TimeDelta(strTimerTag);
 }
 
+HRESULT CGameInstance::Open_Level(_uint iLevelID, CLevel* pNewLevel)
+{
+	if (nullptr == m_pLevel_Manager)
+		return E_FAIL;
+
+	return m_pLevel_Manager->Open_Level(iLevelID, pNewLevel);
+}
+
+CComponent* CGameInstance::Find_Component(_uint iLayerLevelIndex, const _wstring& strLayerTag, const _wstring& strComponentTag, _uint iIndex)
+{
+	return m_pObject_Manager->Get_Component(iLayerLevelIndex, strLayerTag, strComponentTag, iIndex);
+}
+
+HRESULT CGameInstance::Add_GameObject_ToLayer(_uint iLayerLevelIndex, const _wstring& strLayerTag, _uint iPrototypeLevelIndex, const _wstring& strPrototypeTag, void* pArg)
+{
+	if (nullptr == m_pObject_Manager)
+		return E_FAIL;
+
+	return m_pObject_Manager->Add_GameObject_ToLayer(iLayerLevelIndex, strLayerTag, iPrototypeLevelIndex, strPrototypeTag, pArg);
+}
+
+HRESULT CGameInstance::Add_Prototype(_uint iPrototypeLevelIndex, const _wstring& strPrototypeTag, CBase* pPrototype)
+{
+	if (nullptr == m_pPrototype_Manager)
+		return E_FAIL;
+
+	return m_pPrototype_Manager->Add_Prototype(iPrototypeLevelIndex, strPrototypeTag, pPrototype);
+}
+
+CBase* CGameInstance::Clone_Prototype(PROTOTYPE ePrototype, _uint iPrototypeLevelIndex, const _wstring& strPrototypeTag, void* pArg)
+{
+	if (nullptr == m_pPrototype_Manager)
+		return nullptr;
+
+	return m_pPrototype_Manager->Clone_Prototype(ePrototype, iPrototypeLevelIndex, strPrototypeTag, pArg);
+}
+
+HRESULT CGameInstance::Add_RenderGroup(RENDERGROUP eRenderGroup, CGameObject* pRenderObject)
+{
+	return m_pRenderer->Add_RenderGroup(eRenderGroup, pRenderObject);
+}
+
 void CGameInstance::Release_Engine()
 {
 	Release();
+	Safe_Release(m_pRenderer);
+	Safe_Release(m_pObject_Manager);
+	Safe_Release(m_pLevel_Manager);
 	Safe_Release(m_pTimer_Manager);
 	Safe_Release(m_pGraphic_Device);
 }
--- Framework/Engine/Public/GameInstance.h
+++ Framework/Engine/Public/GameInstance.h
@@ -1,6 +1,5 @@
 #pragma once
-#include "Engine_Defines.h"
-#include "Base.h"
+#include "Prototype_Manager.h"
 
 NS_BEGIN(Engine)
 class ENGINE_DLL CGameInstance final : public CBase
@@ -33,13 +32,39 @@ public:
 	void			Compute_TimeDelta(const _wstring& strTimerTag);
 #pragma endregion
 
+#pragma region LEVEL_MANAGER
+public:
+	HRESULT			Open_Level(_uint iLevelID, class CLevel* pNewLevel);
+#pragma endregion
+
+#pragma region OBJECT_MANAGER
+public:
+	class CComponent*	Find_Component(_uint iLayerLevelIndex, const _wstring& strLayerTag, const _wstring& strComponentTag, _uint iIndex = 0);
+	HRESULT				Add_GameObject_ToLayer(_uint iLayerLevelIndex, const _wstring& strLayerTag, _uint iPrototypeLevelIndex, const _wstring& strPrototypeTag, void* pArg = nullptr);
+#pragma endregion
+
+#pragma region PROTOTYPE_MANAGER
+public:
+	HRESULT				Add_Prototype(_uint iPrototypeLevelIndex, const _wstring& strPrototypeTag, class CBase* pPrototype);
+	class CBase*		Clone_Prototype(PROTOTYPE ePrototype, _uint iPrototypeLevelIndex, const _wstring& strPrototypeTag, void* pArg = nullptr);
+#pragma endregion
+
+#pragma region RENDERER
+public:
+	HRESULT				Add_RenderGroup(RENDERGROUP eRenderGroup, class CGameObject* pRenderObject);
+#pragma endregion
+
 private:
 	class CGraphic_Device*		m_pGraphic_Device = { nullptr };
 	class CTimer_Manager*		m_pTimer_Manager = { nullptr };
+	class CLevel_Manager*		m_pLevel_Manager = { nullptr };
+	class CObject_Manager*		m_pObject_Manager = { nullptr };
+	class CPrototype_Manager*	m_pPrototype_Manager = { nullptr };
+	class CRenderer*			m_pRenderer = { nullptr };
 
 public:
-	void			Release_Engine();
-	virtual void	Free() override;
+	void						Release_Engine();
+	virtual void				Free() override;
 
 };
 
